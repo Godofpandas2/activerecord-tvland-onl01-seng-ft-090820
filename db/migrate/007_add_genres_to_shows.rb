6ddb37339ef7cb89c18c702d00dc342b57e1e9e5
David Hutchison <dhutchis68516@gmail.com>
2020-10-09 19:35:55 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_genres_to_shows.rb
+++ db/migrate/007_add_genres_to_shows.rb
@@ -1,7 +1,8 @@
-class AddGenresToShows < ActiveRecord::Migration[5.1]
+class AddAttributesToShows < ActiveRecord::Migration[5.1]
 
   def change
     add_column :shows, :genres, :string
-    add_column :shows, :day, :string
+    add_column :shows, :days, :string
+    add_column :shows, :season, :string
   end
 end
